a6ca28e346d3076b6c2c1f2e0ffb57d06fbffecb
fhafshin <fhafshin@gmail.com>
2025-02-22 23:59:24 +0330

    controller

--- src/main/java/ir/mobin/studentspringboot/controller/CourseController.java
+++ src/main/java/ir/mobin/studentspringboot/controller/CourseController.java
@@ -1,4 +1,45 @@
 package ir.mobin.studentspringboot.controller;
 
+import ir.mobin.studentspringboot.entity.Course;
+import ir.mobin.studentspringboot.entity.Professor;
+import ir.mobin.studentspringboot.service.CourseService;
+import ir.mobin.studentspringboot.service.ProfessorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/course")
+@AllArgsConstructor
 public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Course> save(@RequestBody Course course) {
+        return ResponseEntity.ok(courseService.save(course));
+    }
+
+    @GetMapping("/find-all")
+    public List<Course> findAll() {
+        return courseService.findAll();
+    }
+
+    @GetMapping("/find-one/{id}")
+    public ResponseEntity<Course> findOne(@PathVariable Long id) {
+
+        return ResponseEntity.ok(courseService.findById(id));
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        courseService.delete(id);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Course> update(@RequestBody Course course) {
+        return ResponseEntity.ok(courseService.update(course));
+    }
 }
--- src/main/java/ir/mobin/studentspringboot/controller/StudentController.java
+++ src/main/java/ir/mobin/studentspringboot/controller/StudentController.java
@@ -1,4 +1,45 @@
 package ir.mobin.studentspringboot.controller;
 
+import ir.mobin.studentspringboot.entity.Course;
+import ir.mobin.studentspringboot.entity.Student;
+import ir.mobin.studentspringboot.service.CourseService;
+import ir.mobin.studentspringboot.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/student")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        return ResponseEntity.ok(studentService.save(student));
+    }
+
+    @GetMapping("/find-all")
+    public List<Student> findAll() {
+        return studentService.findAll();
+    }
+
+    @GetMapping("/find-one/{id}")
+    public ResponseEntity<Student> findOne(@PathVariable Long id) {
+
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        studentService.delete(id);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Student> update(@RequestBody Student student) {
+        return ResponseEntity.ok(studentService.update(student));
+    }
 }
